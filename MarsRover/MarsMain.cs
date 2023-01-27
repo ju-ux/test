27d0a20fc94e4fb47645f231feefc61df4a91863
Zerdanes <mathieu.papazian46@gmail.com>
2023-01-27 15:08:13 +0100

    rover

--- MarsRover/MarsMain.cs
+++ MarsRover/MarsMain.cs
@@ -10,37 +10,64 @@ class MarsMain
 
 
         Rover rover = new RoverBuilder()
-               .SurUnePlanète(planète => planète.ToriqueDeTailleX(4))
+               .SurUnePlanète(planète => planète.ToriqueDeTailleX(4).AyantUnObstacle(0, 2))
                .Orienté(Orientation.Nord)
                .Positionné(0, 0)
                .Build();
+        Console.WriteLine("Entrez plusieurs directions à la suite entre 'a', 'g', 'd'");
         String commandLine = Console.ReadLine();
         char[] commandsToChars = commandLine.ToCharArray();
+        Point positionActuelle = new Point(0, 0);
 
         for(int i = 0;i< commandsToChars.Length;i++)
         {
             if (commandsToChars[i].ToString().ToLower().Equals("a"))
             {
-                
-                Console.WriteLine("ma position est : " + rover.Traiter(new AvancerCommande()).Position);
-                continue;
+                positionActuelle = rover.Traiter(new AvancerCommande()).Position;
+                if (positionActuelle != rover.Traiter(new AvancerCommande()).Position)
+                {
+                    Console.WriteLine("ma position est : " + rover.Traiter(new AvancerCommande()).Position);
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine("Je suis face à un obstacle position actuelle : " + positionActuelle);
+                    break;
+                }
             }
             if (commandsToChars[i].ToString().ToLower().Equals("g"))
             {
-                
-                Console.WriteLine("ma position est : " + rover.Traiter(new TourneAGaucheCommande()).Position);
-                continue;
+                positionActuelle = rover.Traiter(new TourneAGaucheCommande()).Position;
+                if (positionActuelle != rover.Traiter(new TourneAGaucheCommande()).Position)
+                {
+                    Console.WriteLine("ma position est : " + rover.Traiter(new TourneAGaucheCommande()).Position);
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine("Je suis face à un obstacle position actuelle : " + positionActuelle);
+                    break;
+                }
             }
             if (commandsToChars[i].ToString().ToLower().Equals("d"))
             {
-                Console.WriteLine("ma position est : " + rover.Traiter(new TournerADroiteCommande()).Position);
-                continue;
+                positionActuelle = rover.Traiter(new TournerADroiteCommande()).Position;
+                if (positionActuelle != rover.Traiter(new TournerADroiteCommande()).Position)
+                {
+                    Console.WriteLine("ma position est : " + rover.Traiter(new TournerADroiteCommande()).Position);
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine("Je suis face à un obstacle position actuelle : " + positionActuelle);
+                    break;
+                }
             }
         }
 
         
        
-
+        Console.ReadKey();
     }
    
 
